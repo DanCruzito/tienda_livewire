9344b6ad4dc47d8a35b28f7c3d9d83536dbab5f5
DanCruzito <dancruzito@gmail.com>
2022-08-28 10:36:44 -0400

    edit-delete

--- app/Http/Livewire/Admin/ShowProducto.php
+++ app/Http/Livewire/Admin/ShowProducto.php
@@ -2,24 +2,76 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Categoria;
 use App\Models\Producto;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class ShowProducto extends Component
 {
+    use WithFileUploads;
     use WithPagination;
     public $search;
-   // public $open = true;
+    public $open_edit = false;
+    public $open_delete = false;
+    public $imagen;
 
-   protected $listeners = ['render'];
-      
+    protected $listeners = ['render'];
+
+    protected $rules = [
+        'producto.nombre' => 'required|min:6',
+        'producto.descripcion' => 'required|max:20',
+        'producto.categoria_id' => 'required',
+        'producto.precio' => 'required',
+        'producto.stock' => 'required'
+    ];
+
+    public function mount()
+    {
+        $this->producto = new Producto();
+    }
+
+    public function edit(Producto $producto)
+    {
+        $this->producto = $producto;
+        $this->open_edit = true;
+    }
+
+    public function update()
+    {
+        $rules = $this->rules;
+        $this->validate($rules);
+        if ($this->imagen) {
+            //La imagen anterior debe eliminarse
+            Storage::delete($this->producto->imagen);
+            $this->producto->imagen = $this->imagen->store('productos');
+        }
+        $this->producto->save();
+        $this->reset('open_edit');
+    }
+
     public function render()
     {
-        $productos = Producto::where('nombre','like','%'.$this->search.'%')
-                    ->orwhere('stock','like','%'.$this->search.'%')
-                    ->orderByDesc('id')
-                    ->paginate(10);
-        return view('livewire.admin.show-producto',compact('productos'));
+        $categorias = Categoria::all();
+        $productos = Producto::where('nombre', 'like', '%' . $this->search . '%')
+            ->orwhere('stock', 'like', '%' . $this->search . '%')
+            ->orderByDesc('id')
+            ->paginate(10);
+        return view('livewire.admin.show-producto', compact('productos', 'categorias'));
+    }
+
+    //Eliminar
+    public function deleteproducto(Producto $producto)
+    {
+        $this->producto = $producto;
+        $this->open_delete = true;
+    }
+
+    public function delete(){
+      Storage::delete($this->producto->imagen);
+      $this->producto->delete();  
+      $this->reset('open_delete');
     }
 }
--- resources/views/livewire/admin/show-producto.blade.php
+++ resources/views/livewire/admin/show-producto.blade.php
@@ -29,6 +29,9 @@
                                 Stock</th>
                             <th
                                 class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                Categoría</th>
+                            <th
+                                class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
                                 Estado</th>
                             <th
                                 class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
@@ -65,6 +68,10 @@
                                 </td>
 
                                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    <div class="text-sm leading-5 text-gray-500">{{ $item->categoria->nombre }}</div>
+                                </td>
+
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                     @if ($item->status == 1)
                                         <span
                                             class="inline-flex px-2 text-xs text font-semibold leading-5 text-white bg-red-400 rounded-full">Borrador</span>
@@ -76,19 +83,23 @@
 
                                 <td
                                     class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                    </svg>
+                                    <a class="cursor-pointer" href="#" wire:click="edit({{ $item }})">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400"
+                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                        </svg>
+                                    </a>
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
+                                    <a class="cursor-pointer" href="#" wire:click="deleteproducto({{ $item }})">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400"
+                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                         @endforeach
@@ -109,5 +120,104 @@
 
         </div>
     </div>
-    
+    <x-jet-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            <div class="font-bold">
+                <span>Actualizar Producto</span>
+            </div>
+
+        </x-slot>
+
+        <x-slot name="content">
+            {{-- Imagen --}}
+            @if ($imagen)
+                <img src="{{ $imagen->temporaryUrl() }}">
+            @else
+                <img src="{{ Storage::url($producto->imagen) }}">
+            @endif
+            {{-- Nombre --}}
+            <div class="mb-4">
+                <x-jet-label value="Nombre del Producto" />
+                <x-jet-input class="w-full" type="text" wire:model="producto.nombre" />
+                <x-jet-input-error for="producto.nombre" />
+            </div>
+            {{-- Categoria --}}
+            <div class="mb-4">
+                <x-jet-label value="Categoria" />
+                <select
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    wire:model="producto.categoria_id">
+                    <option value="">Seleccionar una categoría</option>
+                    @foreach ($categorias as $categoria)
+                        <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
+                    @endforeach
+
+                </select>
+                <x-jet-input-error for="producto.categoria_id" />
+            </div>
+            {{-- Descripcion --}}
+            <div class="mb-4">
+                <x-jet-label value="Descripción" />
+                <textarea rows="3"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    wire:model="producto.descripcion">
+                </textarea>
+                <x-jet-input-error for="producto.descripcion" />
+            </div>
+            {{-- Precio --}}
+            <div class="mb-4">
+                <x-jet-label value="Precio" />
+                <x-jet-input class="w-full" type="number" wire:model="producto.precio" />
+                <x-jet-input-error for="producto.precio" />
+            </div>
+            {{-- Stock --}}
+            <div class="mb-4">
+                <x-jet-label value="Stock" />
+                <x-jet-input class="w-full" type="number" wire:model="producto.stock" />
+                <x-jet-input-error for="producto.stock" />
+            </div>
+
+            {{-- Imagen --}}
+            <div class="mb-4">
+                <span>{{ $this->imagen }}</span>
+                <x-jet-label value="Subir Imagen" />
+                <x-jet-input class="w-full" type="file" wire:model="imagen" />
+                <x-jet-input-error for="imagen" />
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_edit',false)" wire:loading.attr="disabled">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                Actualizar
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+    {{-- Modal Eliminar --}}
+    <x-jet-confirmation-modal wire:model="open_delete">
+        <x-slot name="title">
+            <span class="font-bold text-red-600 text-2xl">Eliminar Producto</span>
+            
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="font-bold text-xl">
+                Está seguro de eliminar este producto? Esta acción no podrá ser revertida!!!
+            </div>
+            
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_delete',false)" wire:loading.attr="disabled">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="delete" wire:loading.attr="disabled">
+                Eliminar Producto
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-confirmation-modal>
 </div>
